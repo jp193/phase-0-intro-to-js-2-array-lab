077670e5ebc4de62cb9129ca801deadcf49f7907
Jamal Portericker <jportericker@yahoo.com>
2023-04-25 02:06:23 -0400

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+function appendCat(name) {
+  const list = cats.slice()
+  list.push(name)
+  return list
+}
+
+function prependCat(name) {
+  const newCat = [...cats]
+  newCat.unshift(name)
+    return newCat
+}
+function removeLastCat() {
+  const catArry = [...cats]
+  catArry.pop()
+  return catArry
+}
+  
+function removeFirstCat() {
+  const catPop = [...cats]
+  catPop.shift()
+  return catPop
+}
